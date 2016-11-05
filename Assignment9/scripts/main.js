9ed9f2aaedd5a5a74e1c0e0f07bfda66bdc991c1
briana olson <brianaselesteo@gmail.com>
2016-11-04 21:24:45 -0500

    final

--- Assignment9/scripts/main.js
+++ Assignment9/scripts/main.js
@@ -1,22 +1,59 @@
+function getRandomNumber() {
+  return Math.floor(Math.random() * (100 - 1)) + 1;
+}
+
+/**part one */
+var name = 'Briana olson';
+document.getElementById ('partOne').innerHTML = name;
+
+/** part two*/
+var foo = "I forgot to do this"
+ var foo = 'This is not an integer';
+document.getElementById ('partTwo').innerHTML = foo;
+
+/**part three */
+
+var partThree = 5; 
+partThree *= 14;
+partThree += 10;
+partThree -= 25;
+document.getElementById ('partThree').innerHTML = partThree;
+
+/** part four */
+var fullName = 'fname' + ' ' + 'lname';
+document.getElementById('partFour').innerHTML = fullName;
 
 
+/**part five */
+
+var myHtml = '';
+myHtml = '<h2> Title </h2>'
+myHtml += '<p> Lorem ipsum dolor </p>'
+myHtml += '<h3>Pellentesque habitant morbi </h3>'
+myHtml += '<p> Morbi in sem quis dui placerat ornare </p>'
+document.getElementById ('partFive') .innerHTML = myHtml;
+
+/** part six */
+
+var part6Number = getRandomNumber ()
+var part6Result
+if (part6Number > 50) {document.getElementById('part6Result').innerHTML = 'Above 50'
+}  else if ( part6Number <= 50){ document.getElementById('part6Result').innerHTML = 'Below or equal to 50'
+} 
+document.getElementById('part6Number').innerHTML = part6Number;
 
 
-
-
-
-
-
-
-
-
-
-var part6Number =getRandomNumber(); 
 
 /**
  *      Part 7
  */
 var part7Number = getRandomNumber();
+var part7Result 
+if (part7Result < 25) {document.getElementById('part7Result').innerHTML = 'Below 25'}
+else if (part7Number >=25 && part7Number <50 ) {document.getElementById('part7Result').innerHTML = 'Between 25 and 50'}
+else if ( part7Number >= 50 && part7Number < 75 ) {document.getElementById('part7Result').innerHTML = 'Between 50 and 75'}
+else {document.getElementById('part7Number').innerHTML = 'Above 75'}
+
 
 
 
--- Exercise2/scripts/main.js
+++ Exercise2/scripts/main.js
@@ -1,6 +1,40 @@
 /*******Part 1***********/
 //Place all of your part 1 code here
+var x= 120;
+var y= 90;
 
+var z = x > y;
+console.log ('z' + z );
+console.log (z);
+
+/////////////////
+ console.log ('start math');
+ x = 10;
+ x += 25;
+ console.log (x);
+ x += 35;
+ console.log (x);
+ x = x / 30;
+
+ console.log ('end math');
+ /////////////////////////
+
+ var one = 1;
+  if (one = 1){
+      console.log ('true');
+
+  }else{ console.log ('false'); }
+
+
+//Declaring a function
+    function sayHello(message1, message2){
+        console.log(message1);
+        console.log(message2);
+    }
+
+    //Executing function
+    sayHello('hello', 'world'); 
+ 
 
 
 
