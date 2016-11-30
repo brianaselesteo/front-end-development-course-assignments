7b3f0d6cb2636abca358a33e81dc1da7eb935ed4
briana olson <brianaselesteo@gmail.com>
2016-11-29 20:18:19 -0600

    15

--- Assignment15/scripts/main.js
+++ Assignment15/scripts/main.js
@@ -1,10 +1,37 @@
 $(document).ready(function(){
 
-     ///// Code goes here
+
+$("#btnOne").on("click", buttonclicked);
+function buttonclicked(){
+  alert("it works");
+};
+
+/* alternative way
+$("#btnOne").on("click",function buttonclicked()
+{alert("hello");
+  });*/
 
 
+$("#btnTwo").on("click", update);
+function update() {$(".update-html").html("<h3>Hello World!</h3>");
+}
 
 
+$("#btnThree").on("click", three);
+function three() {$("#text-update").val("Hello World");
+}
 
+
+$("#btnCopyHtml").on("click", part4);
+function part4 () {
+var copiedHtml = $(".copy-html").html()
+$(".paste-html").html(copiedHtml);
+}
+
+$("#btnCopyValues").on("click", part6);
+function part6 () {
+  var copiedValue = $("#copy-text").val()
+  $("#paste-text").val(copiedValue);
+}
 
 });
