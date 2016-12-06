e4ded3e637a7972cf077d8d2a98129fada3dc3c7
briana olson <brianaselesteo@gmail.com>
2016-12-06 17:29:26 -0600

    part1again

--- Assignment16/scripts/main.js
+++ Assignment16/scripts/main.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){
 
-$('#btnShow').on("click", function() {
+
+('#btnShow').on("click", function() {
   $("section>aside").show()
   console.log("btnShow");
 });
 
+$(document).ready(function () {
+    $('.part3').removeClass('hidden');
+});
+
+$('#btnFadeIn').fadeIn()
 
 });
