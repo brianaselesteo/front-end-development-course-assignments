f195a5a828c038892f604bdb9557661c544b429e
briana olson <brianaselesteo@gmail.com>
2016-12-09 22:20:45 -0600

    assignment18

--- Assignment18/scripts/main.js
+++ Assignment18/scripts/main.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
-    $('input').on('blur', function () {
+    $('input, textarea').on('blur', function () {
         var input = $(this);
         validateRequiredInput(input)
 
         if (input.data('isemail') === true) {
-            validateEmail();
+            validateEmail(input);
         }
 
         if (input.attr('type') === 'password') {
@@ -36,7 +36,7 @@ function validateRequiredInput(input) {
     var val = input.val();
 
     //Find the required input span.
-    var msgSpan = input.closest("input-group").find('.requiredMsg');
+    var msgSpan = input.closest(".input-group").find('.requiredMsg');
     if (val === '') {
         msgSpan.fadeIn();
         input.addClass('invalid');
@@ -60,7 +60,7 @@ function validateEmail(email) {
     if (emailValue !== '') {
         if (emailValue.indexOf('@') > -1 && emailValue.indexOf('.com') > -1) {
             emailErrorMsg.fadeOut();
-            email.removeClass('invald');
+            email.removeClass('invalid');
         } else {
             emailErrorMsg.fadeIn();
             email.addClass('invalid');
@@ -95,7 +95,7 @@ function validatePassword() {
 
     }
 
-    //}
+    }
 }
 
 
@@ -108,10 +108,10 @@ function validateAllInputs() {
     for (var i = 0; i < allInputs.length; i++) {
         var currentInput = allInputs.eq(i);
 
-        validateInput(currentInput);
+        validateRequiredInput(currentInput);
 
-        if (input.data('isemail') === true) {
-            validateEmail();
+        if (currentInput.data('isemail') === true) {
+            validateEmail(currentInput);
         }
          if (input.attr('type') === 'password') {
             validatePassword();
