28956e61391152581789eb47588914f943c75309
briana olson <brianaselesteo@gmail.com>
2017-01-03 17:36:15 -0600

    assignemnt20

--- Assignment20/scripts/main.js
+++ Assignment20/scripts/main.js
@@ -1,4 +1,29 @@
 $(document).ready(function () {
 
 
+  $('#buttonsubmit').on('click', submitForm);
+  $('#buttonreset').on('click', resetForm);
+
+
+  /*$('#yourName').on('blur', validateyourName);
+  $('#yourAnswer').on('blur', validateyourAnswer);*/
+
+
+  function validateName(){
+     var nameElm = $('#yourName');
+if (yourName.value=== "") {
+  errorMsg.push("Your name is required")
+  ;
+
+}
+ }
+
+ function validateAnswer(){
+     var answerElm = $('#yourAnswer');
+
+     function resetForm(){
+     }
+
+    function submitForm(){
+    }
 });
